5386dccc8e601cfafe39ab7867e30fa5f8e8e07f
Amir Panahandeh <amirpanahandeh@yahoo.com>
2024-10-01 17:19:24 +0330

    Upgrade to Flutter 3.24

--- lib/src/widgets/timeline_scroll_view.dart
+++ lib/src/widgets/timeline_scroll_view.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart' hide TwoDimensionalScrollable;
+import 'package:flutter/widgets.dart';
 
 import '../utils/child_delegate.dart';
 import '../utils/model.dart';
 import 'viewport.dart';
-import 'two_dimensional_scrollable.dart';
 
 class TimelineScrollView<T extends TimelineItem>
     extends TwoDimensionalScrollView {
   final double columnWidth, rowHeight;
-  final HitTestBehavior hitTestBehavior;
 
   const TimelineScrollView({
     super.key,
@@ -25,7 +23,7 @@ class TimelineScrollView<T extends TimelineItem>
     super.dragStartBehavior,
     super.diagonalDragBehavior,
     super.keyboardDismissBehavior,
-    this.hitTestBehavior = HitTestBehavior.opaque,
+    super.hitTestBehavior,
   });
 
   @override
@@ -43,70 +41,4 @@ class TimelineScrollView<T extends TimelineItem>
         cacheExtent: cacheExtent,
         clipBehavior: clipBehavior,
       );
-
-  @override
-  Widget build(BuildContext context) {
-    assert(axisDirectionToAxis(verticalDetails.direction) == Axis.vertical,
-        'TwoDimensionalScrollView.verticalDetails are not Axis.vertical.');
-    assert(axisDirectionToAxis(horizontalDetails.direction) == Axis.horizontal,
-        'TwoDimensionalScrollView.horizontalDetails are not Axis.horizontal.');
-
-    ScrollableDetails mainAxisDetails = switch (mainAxis) {
-      Axis.vertical => verticalDetails,
-      Axis.horizontal => horizontalDetails,
-    };
-
-    final bool effectivePrimary = primary ??
-        mainAxisDetails.controller == null &&
-            PrimaryScrollController.shouldInherit(
-              context,
-              mainAxis,
-            );
-
-    if (effectivePrimary) {
-      // Using PrimaryScrollController for mainAxis.
-      assert(
-          mainAxisDetails.controller == null,
-          'TwoDimensionalScrollView.primary was explicitly set to true, but a '
-          'ScrollController was provided in the ScrollableDetails of the '
-          'TwoDimensionalScrollView.mainAxis.');
-      mainAxisDetails = mainAxisDetails.copyWith(
-        controller: PrimaryScrollController.of(context),
-      );
-    }
-
-    final TwoDimensionalScrollable scrollable = TwoDimensionalScrollable(
-      horizontalDetails: switch (mainAxis) {
-        Axis.horizontal => mainAxisDetails,
-        Axis.vertical => horizontalDetails,
-      },
-      verticalDetails: switch (mainAxis) {
-        Axis.vertical => mainAxisDetails,
-        Axis.horizontal => verticalDetails,
-      },
-      diagonalDragBehavior: diagonalDragBehavior,
-      viewportBuilder: buildViewport,
-      dragStartBehavior: dragStartBehavior,
-      hitTestBehavior: hitTestBehavior,
-    );
-
-    final Widget scrollableResult = effectivePrimary
-        // Further descendant ScrollViews will not inherit the same PrimaryScrollController
-        ? PrimaryScrollController.none(child: scrollable)
-        : scrollable;
-
-    if (keyboardDismissBehavior == ScrollViewKeyboardDismissBehavior.onDrag) {
-      return NotificationListener<ScrollUpdateNotification>(
-        child: scrollableResult,
-        onNotification: (ScrollUpdateNotification notification) {
-          final FocusScopeNode focusScope = FocusScope.of(context);
-          if (notification.dragDetails != null && focusScope.hasFocus) {
-            focusScope.unfocus();
-          }
-          return false;
-        },
-      );
-    }
-    return scrollableResult;
-  }
 }
